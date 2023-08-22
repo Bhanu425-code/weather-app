d60112a7db88c5c76ad6911b522024738de42cf4
Bhanu Shree Rekha Gullapalli <84399992+Bhanu425-code@users.noreply.github.com>
2023-08-22 22:12:01 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '18f076497emsh389c4a77bf1ce76p166821jsn50db11634855',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="3be28164f73fa1b8dbdbd290496f92b9";
